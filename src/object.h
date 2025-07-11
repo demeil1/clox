5805c9aa55a95ececfd346d6d4311fc4a7d69f3c
demeil1 <demeil2005@gmail.com>
2025-07-11 16:26:40 -0500

    supporting global variable assignement

--- src/object.h
+++ src/object.h
@@ -8,7 +8,7 @@
 
 #define IS_STRING(value)	isObjType(value, OBJ_STRING)
 
-#define AS_STRING(value)	((ObjString*)AS_OBJ(value))
+#define AS_STRING(value)    ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)	(((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
